250a5d5a6d4bf4f4739d8934e0613548525db536
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-04-13 16:57:56 -0500

    4-13-23

--- db/migrate/20230413213953_add_order_item_to_orders.rb
+++ db/migrate/20230413213953_add_order_item_to_orders.rb
@@ -1,5 +1,5 @@
 class AddOrderItemToOrders < ActiveRecord::Migration[7.0]
   def change
-    add_reference :orders, :order, null: false, foreign_key: true
+    add_reference :order_items, :orders, null: false, foreign_key: true
   end
 end
