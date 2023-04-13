94b92bf847bab56779ecb622745e9f0a8dec7d02
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-04-13 16:31:23 -0500

    added product and models 4-13-23

---
+++ db/migrate/20230413212919_create_order_items.rb
@@ -0,0 +1,10 @@
+class CreateOrderItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_items do |t|
+      t.integer :quantity
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
