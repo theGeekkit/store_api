ac613aa0b78b83ceb875ee5c766f557f9eab0075
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-04-13 16:44:37 -0500

    4-13-23

--- db/migrate/20230413205209_create_products.rb
+++ db/migrate/20230413205209_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 15, scale: 2
 
       t.timestamps
     end
